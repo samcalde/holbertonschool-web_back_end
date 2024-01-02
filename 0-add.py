4d4532db39e9b64de3b7850cfb5d32b37c728825
Samuel Calderon <s.calderon0624@gmail.com>
2024-01-02 12:01:40 -0500

    task 0 added documentation

--- 0-add.py
+++ 0-add.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+"""
+type-annotated function
+"""
+
 def add(a: float, b: float) -> float:
+    """
+    Adds two numbers and returns the sum in float
+    """
     sum = a + b
     return sum
