f92134f3d6ed65ead55c7a1e6c4bab49acfd7d46
Samuel Calderon <s.calderon0624@gmail.com>
2024-01-04 11:24:49 -0500

    format test 1

--- 0-add.py
+++ 0-add.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
-"""
+'''
 Add type-annotated function
-"""
+'''
+
 
 def add(a: float, b: float) -> float:
-    """
-    Adds two numbers and returns the sum in float
-    """
-    return a + b
+    '''
+     Adds two numbers and returns the sum in float
+    '''
+    return (a + b)
