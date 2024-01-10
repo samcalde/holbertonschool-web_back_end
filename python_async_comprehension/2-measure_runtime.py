6add2d3e0165bc5429b7fe673cd235c1161f958f
Samuel Calderon <s.calderon0624@gmail.com>
2024-01-10 11:55:06 -0500

    task 2 test 1

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""
+Async comprehension
+"""
+
+from typing import List
+import time
+import asyncio
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> List[float]:
+    """
+    collect 10 random numbers using an async comprehensing
+    over async_generator, then return the 10 random numbers.
+    """
+    start = time.perf_counter()
+    tasks = [async_comprehension() for i in range(4)]
+    await asyncio.gather(*tasks)
+    total_time = time.perf_counter() - start
+    return total_time
