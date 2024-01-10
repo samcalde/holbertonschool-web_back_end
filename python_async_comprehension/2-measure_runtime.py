a845075ab8b27b1899bb817a65cd982d0822b9ba
Samuel Calderon <s.calderon0624@gmail.com>
2024-01-10 11:57:45 -0500

    task 2 test 2

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -4,13 +4,12 @@
 Async comprehension
 """
 
-from typing import List
 import time
 import asyncio
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime() -> List[float]:
+async def measure_runtime() -> float:
     """
     collect 10 random numbers using an async comprehensing
     over async_generator, then return the 10 random numbers.
