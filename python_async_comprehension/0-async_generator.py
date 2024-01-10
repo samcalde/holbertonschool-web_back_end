752f6fda5c5e65ab8ac8db8ba5b852057e277bba
Samuel Calderon <s.calderon0624@gmail.com>
2024-01-10 11:19:48 -0500

    task 0 test 4

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -9,7 +9,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator:
+async def async_generator() -> Generator[float, None, None]:
     """
     yield a random number between 0 and 10 (x10)
     """
