f5432308eaacd2c6034bebaaa86f33448988e455
Samuel Calderon <s.calderon0624@gmail.com>
2024-01-12 00:29:09 -0500

    task 10 test 1

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+Using Mongo through python
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    changes all topics of a school document based on the name
+    """
+    school = {'name': name}
+    add_topic = {'$set': {'topics': topics}}
+    mongo_collection.updateMany(school, add_topic)
