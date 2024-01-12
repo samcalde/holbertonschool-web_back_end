0296f36bb3d1825b7b7169446c35cd75eca68af2
Samuel Calderon <s.calderon0624@gmail.com>
2024-01-12 01:23:41 -0500

    task 12 test 1

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 
 """
-Using Mongo through python
+Provides some stats about Nginx logs stored in MongoDB
 """
 
+from pymongo import MongoClient
 
-def schools_by_topic(mongo_collection, topic):
-    """
-    function that returns the list of school having a specific topic:
-    """
-    mongo_collection.find({'topic': topic})
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+    log_amount = collection.count_documents()
+    print(f"{log_amount} logs")
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    for method in methods:
+        current_method_amount = collection.count_documents({'method': method})
+        print(f"\tmethod {method}= {current_method_amount}")
+    last_line = collection.count_documents({'method': 'GET', 'path': '/status'})
+    print(f"{last_line} status check")
