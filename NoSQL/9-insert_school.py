3ca30c81ee449dcf8cdcf64f88fcbdee24348b19
Samuel Calderon <s.calderon0624@gmail.com>
2024-01-12 00:22:46 -0500

    task 9 test 1

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Using Mongo through python
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    inserts a new document in a collection based on kwargs
+    """
+    document_id = mongo_collection.insert(kwargs)
+    return document_id
