120de1058c23c31a0dfc4a0b63f853bf421e9472
Samuel Calderon <s.calderon0624@gmail.com>
2024-01-17 22:57:52 -0500

    task 10 test 4

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const array = [];
-  for (const idx of array) {
-    array.push(appendString + value);
+  for (const item of array) {
+    array.push(appendString + item);
   }
-
   return array;
 }
