6f8723b2c0e35c3090ace341e964f1c3ce02a901
Samuel Calderon <s.calderon0624@gmail.com>
2024-01-18 01:34:39 -0500

    task 12 test 1

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,3 +1,9 @@
 export default function createReportObject(employeesList) {
-
+  const ret = {
+    allEmployees: employeesList,
+    getNumberOfDepartments(object) {
+      return Object.keys(object).length;
+    },
+  };
+  return ret;
 }
