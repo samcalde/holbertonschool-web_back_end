c4ac2dc036f7072d54f22fa3f5fa3ef99494feea
Samuel Calderon <s.calderon0624@gmail.com>
2024-01-26 21:34:28 -0500

    task 2 test 4

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,13 +1,12 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => {
-      return ({
+    .then(() => {return ({
         status: 200,
         body: 'success',
       });
     })
     .catch(() => Error())
     .finally(() => {
-      console.log('Got a response from the API')
+      console.log('Got a response from the API');
     });
 }
