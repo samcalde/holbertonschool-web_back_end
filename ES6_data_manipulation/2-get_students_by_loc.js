9112ee04f89f457cea080bd2e87717c95029d428
Samuel Calderon <s.calderon0624@gmail.com>
2024-01-30 16:12:24 -0500

    task 2 fix lint

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
 export default function getStudentsByLocation(stList, city) {
-  return stList.filter((student) => student.location == city);
-};
+  return stList.filter((student) => student.location === city);
+}
