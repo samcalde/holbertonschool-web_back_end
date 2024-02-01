5f6392c844126198e15f87b673c1049d5fdd8253
Samuel Calderon <s.calderon0624@gmail.com>
2024-01-31 22:59:45 -0500

    task 6 test lint

--- ES6_data_manipulation/6-set.js
+++ ES6_data_manipulation/6-set.js
@@ -1,6 +1,6 @@
 export default function setFromArray(arrArg) {
-  let retSet = new Set();
-  arrArg.forEach(value => {
+  const retSet = new Set();
+  arrArg.forEach((value) => {
     retSet.add(value);
   });
   return retSet;
